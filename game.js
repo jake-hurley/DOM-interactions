d056fac5a89b85e1dcc0a8d86c0b4d7457dc980b
Jake-Hurley <jshurley97@gmail.com>
2020-04-24 15:14:50 +1200

    add click event and counter for colored dots

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -22,6 +24,10 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue(evt) {
+  evt.target.classList.toggle("blue");
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
 
@@ -33,7 +39,7 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+  displayBlue(totals)
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
@@ -42,4 +48,15 @@ function displayTotals (totals) {
   for (var key in totals) {
     document.getElementById(key + '-total').innerHTML = totals[key]
   }
+}
+
+function hide(evt) {
+  evt.target.classList.toggle("invisible");
+  updateCounts()
+}
+
+function displayBlue (totals) {
+  totals.blue = document.getElementsByClassName("blue").length;
+  totals.green = document.getElementsByClassName("green").length;
+  totals.invisible = document.getElementsByClassName("invisible").length;
 }
